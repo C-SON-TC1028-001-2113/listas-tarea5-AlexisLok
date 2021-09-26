e1ee82651470e1cad69d8640cc7437567c14e103
AlexisLok <a01251886@itesm.mx>
2021-09-26 09:45:33 +0000

    Ejercicio 1 de Tarea 5

--- assignments/08SinDuplicados/src/exercise.py
+++ assignments/08SinDuplicados/src/exercise.py
@@ -1,41 +1,28 @@
 def main():
     #escribe tu código abajo de esta línea
-    n= int(input(""))
-    list=[]
-    con = 0
-    i = 0
-   
- 
+
+
+    n= int(input("")) #Se pide el número de valores en la lista
+    #Se establecen las listas vacias para capturar
+    list=[] 
+    listCom=[]
+    
+    #Se establece un if para conocer si n es un valor aceptado
     if n > 1 :
-        while i < n:
-            valor = input("")
-            list.append(valor)
-            i += 1
         
-        for x in range(0,n+1):
-            valor = list[con]
-            print(valor)
+        #Ciclo para agregar valores a lista
+        for x in range(0,n):
+            valor = input("")
+            listCom.append(valor) #Se llena la lista sin borrar valores
+            if valor not in list: #El if evalua si el valor se encuentra en la lista
+                list.append(valor)
 
-            while con < n:
-                borrar = list[x+1]
-                if borrar == valor:
-                    del list[x+1]
-                con = con +1
-
-        #while n>con:
-         #   valor = list[con]
-            
-          #  while n> x+1:
-           #     if valor == list[x]:
-            #        del list[x]
-             #   x+=1
-            #con += 1
- 
+        #Se imprimen la lista completa y la modificada
+        print(listCom)
         print(list)
         
     else:
-        print("Error")
- 
+        print("Error") 
  
 if __name__=='__main__':
     main()
