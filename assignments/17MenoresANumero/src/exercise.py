1b7b6be0fb02deaba545103796f4f89253517e20
AlexisLok <a01251886@itesm.mx>
2021-09-26 02:10:11 +0000

    Ejercicio 2 de Tarea 5

--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -5,15 +5,16 @@ def main():
     c=0
     i = 0
     list=[]
-    r = int(input(""))
-    c = int(input(""))
-    while i < r*c:
+    r = int(input("")) #Establece renglones de matriz
+    c = int(input("")) #Columnas de matriz
+    #Se realiza un ciclo para tomar los datos de la matriz 
+    while i < r*c: 
         valor = int(input(""))
-        if valor <= 10:
+        if valor < 10: #Si los valores son menores a 10, se agregan a la lista
             list.append(valor)
         i = i + 1
     
-    print(list)
+    print(list) #Se imprime la lista
 
 
 
