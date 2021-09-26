51ffe39df300e121ec50b18024bbeaaa882930c1
AlexisLok <a01251886@itesm.mx>
2021-09-26 02:07:22 +0000

    Ejercicio 2 de Tarea 5

--- assignments/08SinDuplicados/src/exercise.py
+++ assignments/08SinDuplicados/src/exercise.py
@@ -1,8 +1,41 @@
 def main():
     #escribe tu código abajo de esta línea
+    n= int(input(""))
+    list=[]
+    con = 0
+    i = 0
+   
+ 
+    if n > 1 :
+        while i < n:
+            valor = input("")
+            list.append(valor)
+            i += 1
+        
+        for x in range(0,n+1):
+            valor = list[con]
+            print(valor)
 
+            while con < n:
+                borrar = list[x+1]
+                if borrar == valor:
+                    del list[x+1]
+                con = con +1
 
-    pass
-
+        #while n>con:
+         #   valor = list[con]
+            
+          #  while n> x+1:
+           #     if valor == list[x]:
+            #        del list[x]
+             #   x+=1
+            #con += 1
+ 
+        print(list)
+        
+    else:
+        print("Error")
+ 
+ 
 if __name__=='__main__':
     main()
--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -1,7 +1,19 @@
 
 
 def main():
-
+    r=0
+    c=0
+    i = 0
+    list=[]
+    r = int(input(""))
+    c = int(input(""))
+    while i < r*c:
+        valor = int(input(""))
+        if valor <= 10:
+            list.append(valor)
+        i = i + 1
+    
+    print(list)
 
 
 
